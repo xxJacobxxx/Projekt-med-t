e7e8a93aa1c2aaa8a8f2a32c485b668b12fa7554
Jacob <jacob.hansen@elev.ga.ntig.se>
2023-09-06 10:07:14 +0200

    Gjorde multiplikation

--- src/App.java
+++ src/App.java
@@ -3,10 +3,12 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
         Scanner tangbentgord = new Scanner(System.in);
-        System.out.println("Droppa talet bror:");
+        Scanner tb = new Scanner(System.in);
+        System.out.print("Droppa talet bror:");
         int tal1 = tangbentgord.nextInt();
-        System.out.println("Droppa andra talet bram:");
-        int tal2 = tangbentgord.nextInt();
-        System.out.println(tal1+tal2);
+        System.out.print("Droppa andra talet bram:");
+        int tal2 = tb.nextInt();
+        int produkt = tal1*tal2;
+        System.out.println(tal1+"*"+tal2+"="+produkt);
     }
 }
