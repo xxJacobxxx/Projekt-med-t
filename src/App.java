cb06d0b9f7535b129f40d243e06f416bac489233
Jacob <jacob.hansen@elev.ga.ntig.se>
2023-09-06 10:50:41 +0200

    Finat till det och fixat kvot

--- src/App.java
+++ src/App.java
@@ -3,12 +3,23 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
         Scanner tangbentgord = new Scanner(System.in);
-        Scanner tb = new Scanner(System.in);
         System.out.print("Droppa talet bror:");
         int tal1 = tangbentgord.nextInt();
+        tangbentgord.nextLine();
         System.out.print("Droppa andra talet bram:");
-        int tal2 = tb.nextInt();
+        int tal2 = tangbentgord.nextInt();
+                tangbentgord.nextLine();
+        
+        System.out.print("Droppa decimaltalet bror:");
+        double  tal3 = tangbentgord.nextDouble();
+                tangbentgord.nextLine();
+        System.out.print("Droppa andra decimaltalet yäni:");
+        double tal4 = tangbentgord.nextDouble();
+                tangbentgord.nextLine();
+        double kvot = tal3/tal4;
+        System.out.println(tal3+"/"+tal4+"="+kvot);
         int produkt = tal1*tal2;
         System.out.println(tal1+"*"+tal2+"="+produkt);
+        
     }
 }
